837032c0a73019b8edf6c212df74a7a3669cda1a
Constantine-Marx <2522125485@qq.com>
2022-10-18 10:08:41 +0800

    10.18

--- 268/main.go
+++ 268/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+func missingNumber(nums []int) int {
+	n := len(nums)
+	sum := (1 + n) * n / 2
+	sum1 := 0
+	for _, v := range nums {
+		sum1 += v
+	}
+	return sum - sum1
+}
+
 func main() {
-	
+	nums := []int{9,6,4,2,3,5,7,0,1}
+	fmt.Println(missingNumber(nums))
 }
